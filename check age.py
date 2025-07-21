ad257c839ea37e6b9d5a0a14a5c425d2d610c82d
loyaldreams2898 <aaban8928@gmail.com>
2025-07-21 18:16:22 +0500

    initial commit

--- check age.py
+++ check age.py
@@ -2,14 +2,11 @@ print("admission in golf club")
 
 age = int(input("Enter your age: "))
 
-if age < 25:
-    print("You are too young to join the golf club.")
+if age < 10:
+    print("You are not allowed to enrol the class.")
 
-elif (age >= 30 ):
-    print("You allowed to join the golf club.")
+elif (age >= 20 ):
+    print("You are allowed to join the golf club.")
 
-elif (age >= 40 ):
-     print("You are too old to join the golf club.")
-     
 else:
-    print("You are allowed to join the golf club, but you need to pay a higher fee.")
+    print("You are allowed too old to enrol in class.")
